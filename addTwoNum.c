314f2d03dcab3e81f6b06803fd96c7d43673b371
Ovijit Debnath <ovijit8841@gmail.com>
2025-06-04 07:58:34 -0500

    Add files via upload

---
+++ addTwoNum.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+int main()
+
+{
+    int a,b,Add;
+
+    printf("Enter two Numbers :\n");
+
+    scanf("%d%d",&a,&b);
+
+    Add=a+b;
+
+    printf("Addition of two Numbers:%d ",Add);
+
+    return 0;
+
+}
+
+
+
+
